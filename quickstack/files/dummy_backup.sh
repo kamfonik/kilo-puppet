f916cf886cf8c8fc84f093376f5aa7c108c8dea0
Laura Kamfonik <kamfonik@bu.edu>
2016-03-24 10:41:10 -0400

    create dummy backup script

---
+++ quickstack/files/dummy_backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This is a dummy script that can be used on testing nodes that don't actually need to be backed up.
+# It exists so Puppet still has somethign to install, and the cronjob doesn't send out emails that
+# it can't find the backup script.
