63dad4a8f7aa82b7d73c96bf646e18ddad8ede2e
root <root@dev-foreman1.staging.moc.edu>
2016-05-04 13:23:02 -0400

    memory metrics final

--- sensu/files/plugins/memory-metrics.rb
+++ sensu/files/plugins/memory-metrics.rb
@@ -73,7 +73,7 @@ class MemoryGraphite < Sensu::Plugin::Metric::CLI::Graphite
     mem['swapUsedPercentage'] = 100 * mem['swapUsed'] / mem['swapTotal'] if mem['swapTotal'] > 0
 
     memory_used = (100.0 * mem['freeWOBuffersCaches']) / mem['total']
-     top5 = 'ps -eo user,pid,%mem,command --sort=%mem --cols 75 | tail -10'
+     top5 = 'ps -eo user,pid,%mem,command --sort=%mem --cols 75 | tail -10| tac'
      header = 'ps -eo user,pid,%mem,command --sort=%mem| head -n1'
      output = 'ls'
     if config[:threshold].to_f < memory_used
