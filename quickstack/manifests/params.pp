e015855e5682398ae3fb6a61a88c4d3f28c31d74
Jeremy Freudberg <jeremyfreudberg@gmail.com>
2016-07-14 12:35:31 -0400

    fix for ceilometer compute nodes. changes rpc backend to rabbit

--- quickstack/manifests/params.pp
+++ quickstack/manifests/params.pp
@@ -409,6 +409,7 @@ class quickstack::params (
   $ceilometer_keystone_user,
   $ceilometer_keystone_tenant,
   $ceilometer_rabbit_port,
-  $ceilometer_auth_url
+  $ceilometer_auth_url,
+  $ceilometer_backend
 ) {
 }
